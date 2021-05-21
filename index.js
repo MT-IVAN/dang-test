027cbbb719981a44c4272e13c959563011ab4e00
Ivan Eraso <ivanandreseraso@gmail..com>
2021-05-20 19:11:49 -0500

    se inicia el proyecto

---
+++ index.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql');
+const { addListener } = require('nodemon');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database : "ivis",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+con.query('select * from user', (err, results, fields)=>{
+  if(err)
+    throw err;
+    results.forEach(pan => {
+      console.log(pan);
+  });
+});
+
+con.end();
+
+
+const arr = [1,2,3,5,6,7,8,9,0];
+const ans =[];
+for(let i = 0; i< arr.length;i++){
+  if(arr[i]%2==0)
+    {
+      ans.push(arr[i])
+    }
+
+}
+
+arr.forEach(num => console.log(num));
+
+const filtrados = arr.filter(enteros => enteros%2==0);
+console.log(filtrados);
+
+
+
